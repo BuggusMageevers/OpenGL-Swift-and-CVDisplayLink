60d9b878d175ca18be88c12d8d8d43790aefcb28
Myles La Verne Schultz <Myles@ayane-2.local>
2015-02-15 18:44:49 -0500

    Initial Commit

---
+++ SwiftOpenGL/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SwiftOpenGL
+//
+//  Created by Myles La Verne Schultz on 2/15/15.
+//  Copyright (c) 2015 MyKo. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
